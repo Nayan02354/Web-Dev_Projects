bd6e3043ddb56edaab6a6611a3f3d013efb3b627
Nayan <vishwakarmanayan418@gmail.com>
2026-07-20 20:32:13 +0530

    Add password change modal and forgot password modal; implement password reset functionality

--- Cravings_Using_MERN/Client/src/components/Navbar.jsx
+++ Cravings_Using_MERN/Client/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
-import logoLight from "../assets/transparentLogoLight.png";
+// import logoLight from "../assets/transparentLogoLight.png";
 import { useAuth } from "../context/AuthContext";
 import { FaPowerOff } from "react-icons/fa";
 import toast from "react-hot-toast";
@@ -65,8 +65,8 @@ const Navbar = () => {
               />
               <div className="flex flex-col items-start">
                 <span className="text-base">{user?.fullName}</span>
-                <span className="text-xs text-(--color-primary-content)/80">
-                  Customer
+                <span className="text-xs text-(--color-primary-content)/80 uppercase">
+                  {role}
                 </span>
               </div>
             </button>
@@ -100,3 +100,4 @@ const Navbar = () => {
 };
 
 export default Navbar;
+ 
