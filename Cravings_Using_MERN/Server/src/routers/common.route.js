33285a2c88dff4d5d7fe05775b73af6c8758efbc
Nayan <vishwakarmanayan418@gmail.com>
2026-08-18 20:35:30 +0530

    feat: Implement restaurant and rider models; add admin and common routes; enhance restaurant routes with new update functionalities

---
+++ Cravings_Using_MERN/Server/src/routers/common.route.js
@@ -0,0 +1,21 @@
+import express from "express";
+import multer from "multer";
+import {
+  EditUserProfile,
+  UpdateUserPassword,
+} from "../controllers/common.controller.js";
+import { AuthProtect } from "../middlewares/auth.middleware.js";
+
+const Upload = multer();
+const router = express.Router();
+
+router.put(
+  "/edit-profile",
+  AuthProtect,
+  Upload.single("displayPic"),
+  EditUserProfile,
+);
+
+router.patch("/change-password", AuthProtect, UpdateUserPassword);
+
+export default router;
