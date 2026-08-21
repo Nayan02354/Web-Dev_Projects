31ff3c7258137739cc7aa113cff97c6d1965effa
Nayan <vishwakarmanayan418@gmail.com>
2026-08-21 20:43:33 +0530

    feat: Add Home and Login pages; implement navigation buttons for chat and contact

---
+++ Mingo_Chat_App/Server/index.js
@@ -0,0 +1,26 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello from the server!");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+
+
+
+
+
+
+
+
+import http from "http";
+import { Server } from "socket.io";
